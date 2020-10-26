a43d9bfc7e3368e0a1b4b484a821bd64e4a7e5cf
ashaik-incomm <53871896+ashaik-incomm@users.noreply.github.com>
2020-10-26 15:31:14 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div data-app id="app">
+    <VueTable></VueTable>
+  </div>
+</template>
+
+<script>
+import VueTable from './components/VueTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VueTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
